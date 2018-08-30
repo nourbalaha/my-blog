67143da93a4be99f4cd92c5a81c232bf75558160
nourbalaha <nourbalaha0@gmail.com>
2018-08-30 18:36:23 +0900

    added data

--- data.py
+++ data.py
@@ -21,6 +21,111 @@ def Articles() :
             "author":"Mary Smith",
             "created_date":"12-06-2017"
         },
+                {
+            "id":1,
+            "title":"title one",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"Nour Balaha",
+            "created_date":"12-04-2017"
+        },
+        {
+            "id":2,
+            "title":"title two",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"John Doe",
+            "created_date":"12-05-2017"
+        },
+        {
+            "id":3,
+            "title":"title three",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"Mary Smith",
+            "created_date":"12-06-2017"
+        },
+                {
+            "id":1,
+            "title":"title one",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"Nour Balaha",
+            "created_date":"12-04-2017"
+        },
+        {
+            "id":2,
+            "title":"title two",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"John Doe",
+            "created_date":"12-05-2017"
+        },
+        {
+            "id":3,
+            "title":"title three",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"Mary Smith",
+            "created_date":"12-06-2017"
+        },
+                {
+            "id":1,
+            "title":"title one",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"Nour Balaha",
+            "created_date":"12-04-2017"
+        },
+        {
+            "id":2,
+            "title":"title two",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"John Doe",
+            "created_date":"12-05-2017"
+        },
+        {
+            "id":3,
+            "title":"title three",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"Mary Smith",
+            "created_date":"12-06-2017"
+        },
+                {
+            "id":1,
+            "title":"title one",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"Nour Balaha",
+            "created_date":"12-04-2017"
+        },
+        {
+            "id":2,
+            "title":"title two",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"John Doe",
+            "created_date":"12-05-2017"
+        },
+        {
+            "id":3,
+            "title":"title three",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"Mary Smith",
+            "created_date":"12-06-2017"
+        },
+                {
+            "id":1,
+            "title":"title one",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"Nour Balaha",
+            "created_date":"12-04-2017"
+        },
+        {
+            "id":2,
+            "title":"title two",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"John Doe",
+            "created_date":"12-05-2017"
+        },
+        {
+            "id":3,
+            "title":"title three",
+            "body":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam obcaecati quod cum omnis amet ipsa, rem consectetur temporibus asperiores sunt quidem optio perferendis, ullam dolor ex animi atque quos.",
+            "author":"Mary Smith",
+            "created_date":"12-06-2017"
+        },
     ]
     return articles
 
