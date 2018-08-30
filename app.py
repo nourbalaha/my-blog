2cff46122118db2f2acd6c69f8c1642d401186cb
nourbalaha <nourbalaha0@gmail.com>
2018-08-30 12:15:38 +0900

    initail commit

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from data import Articles
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/users'
+db = SQLAlchemy(app)
+Articles = Articles()
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/articles")
+def articles():
+    return render_template("articles.html", articles=Articles)
+
+
+@app.route("/article/<string:id>")
+def article(id):
+    return render_template("article.html", id=id)
+
+
+class RegisterForm(Form):
+    name = StringField("Name", [validators.Length(min=1, max=50)])
+    email = StringField("Email", [validators.Length(min=6, max=50)])
+    username = StringField("Username", [validators.Length(min=4, max=25)])
+    password = PasswordField("Password", [validators.DataRequired(
+    ), validators.EqualTo("confirm", message="Passwords do not match")])
+    confirm = PasswordField("Confirm Password")
+
+class User(db.Model):
+    __users__= "users"
+    id=db.Column("id",db.Integer,primary_key=True)
+    name=db.Column("name",db.Unicode)
+    email=db.Column("email",db.Unicode)
+    username=db.Column("username",db.Unicode)
+    password=db.Column("password",db.Unicode)
+
+    def __init__(self,name,email,username,password):
+        self.name=name
+        self.email=email
+        self.username=username
+        self.password=password
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == "POST" and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        new_user=User(name,email,username,password)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        flash("You are now registered and can log in","success")
+
+        redirect(url_for("index"))
+    return render_template("register.html", form=form)
+
+
+if __name__ == '__main__':
+    app.secret_key="12345"
+    app.run(debug=True)
